9909412f9af8036be373e9e9b17084bd85c1ed41
Paul Paguay <pauljps@yahoo.com>
2016-01-12 19:37:14 -0500

    Agregue clase Prueba

---
+++ redsocial/src/java/ec/edu/unach/red/rnegocio/clases/Prueba.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ec.edu.unach.red.rnegocio.clases;
+
+/**
+ *
+ * @author PAUL
+ */
+public class Prueba {
+    int codigo;
+    String nombre;
+            
+}
